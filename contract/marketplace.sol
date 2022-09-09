5cd24ae71ac97a3a4d4846cbcb9d4636bb861628
Chr0n0sXIII <71567732+Chr0n0sXIII@users.noreply.github.com>
2022-09-09 09:39:31 -0400

    updated Marketplace.sol

--- contract/marketplace.sol
+++ contract/marketplace.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
-
+//dev note: add quantity attribute;
 pragma solidity >=0.7.0 <0.9.0;
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 interface IERC20Token {
   function transfer(address, uint256) external returns (bool);
@@ -11,7 +12,7 @@ interface IERC20Token {
   function allowance(address, address) external view returns (uint256);
 
   event Transfer(address indexed from, address indexed to, uint256 value);
-  event Approval(address indexed OId, address indexed spender, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
 contract Marketplace {
@@ -20,13 +21,18 @@ contract Marketplace {
     address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
 
     struct Product {
-        address payable OId;
-        string title;
+        address payable owner;
+        string name;
         string image;
         string description;
         string location;
-        uint price;
+        Logistics logistics;
+        
+    }
+    struct Logistics{
+        uint quantity;
         uint sold;
+        uint price;
     }
 
     mapping (uint => Product) internal products;
@@ -36,7 +42,9 @@ contract Marketplace {
         string memory _image,
         string memory _description, 
         string memory _location, 
-        uint _price
+        uint _price,
+        uint _quantity
+    
     ) public {
         uint _sold = 0;
         products[productsLength] = Product(
@@ -45,42 +53,68 @@ contract Marketplace {
             _image,
             _description,
             _location,
-            _price,
-            _sold
+            Logistics(
+                _quantity,
+                _sold,
+                _price
+            )
         );
         productsLength++;
     }
-
+    function readLogistics(uint _index)public view returns(string memory){
+            string memory temp;
+            uint val;
+            temp ="Price" ;
+            val = products[_index].logistics.price;
+            temp = string(bytes.concat(bytes(temp) ,": ",bytes(Strings.toString(val))));
+            val = products[_index].logistics.sold;
+            temp = string(bytes.concat(bytes(temp) ,"    Sold: ",bytes(Strings.toString(val))));
+            val = products[_index].logistics.quantity;
+            temp = string(bytes.concat(bytes(temp) ,"    Quantity: ",bytes(Strings.toString(val))));
+        return(
+        temp
+        );
+    }
+    function getLogistics(uint _index) public view returns (
+        uint,
+        uint,
+        uint
+    ){
+        return ( 
+            products[_index].logistics.price,
+            products[_index].logistics.sold,
+            products[_index].logistics.quantity
+        );
+    }
     function readProduct(uint _index) public view returns (
         address payable,
         string memory, 
         string memory, 
         string memory, 
         string memory, 
-        uint, 
-        uint
+        string memory
     ) {
         return (
-            products[_index].OId,
-            products[_index].title, 
+            products[_index].owner,
+            products[_index].name, 
             products[_index].image, 
             products[_index].description, 
             products[_index].location, 
-            products[_index].price,
-            products[_index].sold
+            readLogistics(_index)            
         );
     }
-    
+   
     function buyProduct(uint _index) public payable  {
         require(
           IERC20Token(cUsdTokenAddress).transferFrom(
             msg.sender,
-            products[_index].OId,
-            products[_index].price
+            products[_index].owner,
+            products[_index].logistics.price
           ),
           "Transfer failed."
         );
-        products[_index].sold++;
+        products[_index].logistics.sold++;
+        products[_index].logistics.quantity--;
     }
     
     function getProductsLength() public view returns (uint) {
