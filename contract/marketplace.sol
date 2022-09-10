3c9d6a1e67bf25779e53b29786ee3f0d600409bb
Farzeen Kistnareddy <90398313+FarzeenKist@users.noreply.github.com>
2022-09-10 12:21:10 +0400

    cleanup, fixes and some minor improvements

--- contract/marketplace.sol
+++ contract/marketplace.sol
@@ -1,24 +1,39 @@
 // SPDX-License-Identifier: MIT
-//dev note: add quantity attribute;
 pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 interface IERC20Token {
-  function transfer(address, uint256) external returns (bool);
-  function approve(address, uint256) external returns (bool);
-  function transferFrom(address, address, uint256) external returns (bool);
-  function totalSupply() external view returns (uint256);
-  function balanceOf(address) external view returns (uint256);
-  function allowance(address, address) external view returns (uint256);
+    function transfer(address, uint256) external returns (bool);
 
-  event Transfer(address indexed from, address indexed to, uint256 value);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
+    function approve(address, uint256) external returns (bool);
+
+    function transferFrom(
+        address,
+        address,
+        uint256
+    ) external returns (bool);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address) external view returns (uint256);
+
+    function allowance(address, address) external view returns (uint256);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
 }
 
 contract Marketplace {
+    // event that is emitted when a product is restocked
+    event Restock(uint index, uint amount);
 
-    uint internal productsLength = 0;
-    address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+    uint private productsLength = 0;
+    address internal cUsdTokenAddress =
+        0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
 
     struct Product {
         address payable owner;
@@ -27,25 +42,40 @@ contract Marketplace {
         string description;
         string location;
         Logistics logistics;
-        
     }
-    struct Logistics{
+    struct Logistics {
         uint quantity;
         uint sold;
         uint price;
     }
 
-    mapping (uint => Product) internal products;
+    mapping(uint => Product) private products;
+    mapping(uint => bool) private _exists;
 
+    /// @dev ensures product with id _index exists
+    modifier exists(uint _index) {
+        require(_exists[_index], "Query of nonexistent product");
+        _;
+    }
+
+    /**
+     * @dev creates a new product on the blockchain
+     * @notice the input data must not contain empty values
+     */
     function writeProduct(
-        string memory _name,
-        string memory _image,
-        string memory _description, 
-        string memory _location, 
+        string calldata _name,
+        string calldata _image,
+        string calldata _description,
+        string calldata _location,
         uint _price,
         uint _quantity
-    
     ) public {
+        require(bytes(_name).length > 0, "Empty name");
+        require(bytes(_image).length > 0, "Empty image");
+        require(bytes(_description).length > 0, "Empty description");
+        require(bytes(_location).length > 0, "Empty loca_location");
+        require(_price > 0, "Price needs to be at least one wei");
+        require(_quantity > 0, "Quantity needs to be at least one");
         uint _sold = 0;
         products[productsLength] = Product(
             payable(msg.sender),
@@ -53,70 +83,61 @@ contract Marketplace {
             _image,
             _description,
             _location,
-            Logistics(
-                _quantity,
-                _sold,
-                _price
-            )
+            Logistics(_quantity, _sold, _price)
         );
+        _exists[productsLength] = true;
         productsLength++;
     }
-    function readLogistics(uint _index)public view returns(string memory){
-            string memory temp;
-            uint val;
-            temp ="Price" ;
-            val = products[_index].logistics.price;
-            temp = string(bytes.concat(bytes(temp) ,": ",bytes(Strings.toString(val))));
-            val = products[_index].logistics.sold;
-            temp = string(bytes.concat(bytes(temp) ,"    Sold: ",bytes(Strings.toString(val))));
-            val = products[_index].logistics.quantity;
-            temp = string(bytes.concat(bytes(temp) ,"    Quantity: ",bytes(Strings.toString(val))));
-        return(
-        temp
-        );
+
+    /// @return Product details with _index
+    /// @notice Logistics also returns the logistics of product
+    function readProduct(uint _index) public view exists(_index) returns (Product memory) {
+        return (products[_index]);
     }
-    function getLogistics(uint _index) public view returns (
-        uint,
-        uint,
-        uint
-    ){
-        return ( 
-            products[_index].logistics.price,
-            products[_index].logistics.sold,
-            products[_index].logistics.quantity
-        );
-    }
-    function readProduct(uint _index) public view returns (
-        address payable,
-        string memory, 
-        string memory, 
-        string memory, 
-        string memory, 
-        string memory
-    ) {
-        return (
-            products[_index].owner,
-            products[_index].name, 
-            products[_index].image, 
-            products[_index].description, 
-            products[_index].location, 
-            readLogistics(_index)            
-        );
-    }
-   
-    function buyProduct(uint _index) public payable  {
+
+    /**
+     * @dev allow users to buy a product with id of _index
+     * @notice an Amount for number of products needs to be specified
+     */
+    function buyProduct(uint _index, uint amount) public payable exists(_index) {
+        Product storage currentProduct = products[_index];
         require(
-          IERC20Token(cUsdTokenAddress).transferFrom(
-            msg.sender,
-            products[_index].owner,
-            products[_index].logistics.price
-          ),
-          "Transfer failed."
+            currentProduct.owner != msg.sender,
+            "You can't buy your own product"
         );
-        products[_index].logistics.sold++;
-        products[_index].logistics.quantity--;
+        require(
+            currentProduct.logistics.quantity >= amount,
+            "Amount can't be fulfilled by the current inventory of product"
+        );
+
+        uint newSoldAmount = currentProduct.logistics.sold + amount;
+        uint newQuantity = currentProduct.logistics.quantity - amount;
+
+        currentProduct.logistics.sold = newSoldAmount;
+        currentProduct.logistics.quantity = newQuantity;
+        require(
+            IERC20Token(cUsdTokenAddress).transferFrom(
+                msg.sender,
+                currentProduct.owner,
+                currentProduct.logistics.price * amount
+            ),
+            "Transfer failed."
+        );
     }
-    
+
+    /**
+     * @dev allow products' owners to restock their inventory
+     * @notice an amount needs to be specified
+     */
+    function reStockProduct(uint _index, uint amount) public exists(_index) {
+        Product storage currentProduct = products[_index];
+        require(currentProduct.owner == msg.sender, "Unauthorized caller");
+
+        uint newQuantity = currentProduct.logistics.quantity + amount;
+        currentProduct.logistics.quantity = newQuantity;
+        emit Restock(_index, amount);
+    }
+
     function getProductsLength() public view returns (uint) {
         return (productsLength);
     }
